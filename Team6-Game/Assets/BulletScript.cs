e21a559911d8af22600c0ce81eccd8b33933371d
XateAorora <GamerXD8@yahoo.com>
2019-02-12 10:56:06 -0800

    Add files via upload
    Bullet prefabs, bullet and player script. Retool the player control to account for the new jump button

---
+++ Team6-Game/Assets/BulletScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 600;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        Vector3 direction = transform.up;
+        rb2d.velocity = direction * speed;
+
+        //rb2d.velocity = transform.forward * speed;
+        //Vector2 bulletaim = plyerctrl.transform.rotation * Vector3.forward;
+        //Vector2 bulletaim = new Vector2(0, speed);
+        //rb2d.AddForce(direction * speed);
+        //    transform.rotation = Quaternion.LookRotation(Vector3.forward, movement);
+
+        //Vector2 direction = transform.TransformDirection(Vector3.forward * speed * 100);
+        //rb2d.velocity = transform.forward.normalized * speed;
+        //rb2d.AddForce(transform.up *  speed);
+        //plyerctrl.transform.rotation.LookRotation;
+        //rb2d.velocity = plyerctrl.movement;
+        //transform.position += transform.forward * Time.deltaTime * speed;
+        //rb2d.transform.position += transform.forward * Time.deltaTime * speed;
+        //rb2d.velocity = new Vector2(direction.x * speed * 100f, direction.y * speed * 100f);
+        //rb2d.velocity = bulletaim;
+    }
+
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*if (collision.gameObject.tag == "Player")
+        {
+            Physics2D.IgnoreCollision(collision.collider, this.gameObject.GetComponent<Collider2D>());
+        */
+
+        this.gameObject.SetActive(false);
+        collision.gameObject.SetActive(false);
+    }
+}
---
+++ Team6-Game/Assets/PlayerCtrl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed;
+    public string name;
+    public Rigidbody2D bulletPrefab;
+    public Rigidbody2D chargedbulletPrefab;
+    public float fireRate;
+    public float chargeRate;
+    public float laserRate;
+    public float laserLength;
+
+    private bool isCharging;
+    private bool isLaser;
+    private float laserTimer;
+    private float nextFire;
+    private float nextCharge;
+    private Light playerLight;
+    private Rigidbody2D rigidB;
+
+
+    void Start()
+    {
+        rigidB = GetComponent<Rigidbody2D>();
+        playerLight = GetComponent<Light>();
+        nextCharge = 0;
+        nextFire = fireRate;
+        isCharging = false;
+        isLaser = false;
+        laserTimer = 0;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal_" + name);
+        float moveVertical = Input.GetAxis("Vertical_" + name);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rigidB.AddForce(movement * speed);
+        if (playerLight.range > 5)
+        {
+            playerLight.range -= 0.01f;
+        }
+        if (movement != Vector2.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, movement);
+        }
+        //machine gun spray
+        nextFire += Time.deltaTime;
+        if (laserTimer < laserLength && isLaser == true)
+        {
+            isCharging = false;
+            laserTimer += Time.deltaTime;
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+        else if (Input.GetButton("Jump"))
+        {
+            isLaser = false;
+            if (nextFire > fireRate && isCharging == false)
+            {
+                Vector3 spawn = transform.position + transform.up * 5;
+                Instantiate(bulletPrefab, spawn, transform.rotation);
+                nextFire = 0;
+            }
+            nextCharge += Time.deltaTime;
+            isCharging = true;
+        }
+        else
+        {
+            isCharging = false;
+            if (nextCharge > laserRate/*4 * chargeRate*/)
+            {
+                nextCharge = 0;
+                laserTimer = 0;
+                isLaser = true;
+            }
+            if (nextCharge > chargeRate)
+            {
+                Instantiate(chargedbulletPrefab, transform.position, transform.rotation);
+                nextCharge = 0;
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
